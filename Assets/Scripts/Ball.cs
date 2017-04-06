26aa993350821cbdc725e73985cbe4faa1bd0107
Unknown <bmff11@gmail.com>
2017-04-06 11:26:46 -0400

    Added game over

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,33 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Ball : MonoBehaviour {
 
     public Vector2 startingVelocity = new Vector2(15, -20);
     private Vector3 startingPosition;
     public GameObject gameover;
+    public GameObject youwin;
+    public Text livesValue;
+    public Text pointsValue;
 
     int lives = 3;
+    int points = 0;
 
     void Start() {
+        livesValue.text = lives.ToString();
         startingPosition = transform.position;
         GetComponent<Rigidbody2D>().velocity = startingVelocity;
     }
+
 
 
     void Update() {
         if (transform.position.y < -7.6) {
             GetOut();
         }
-        if (Input.GetButtonDown("Jump")) ;
-        GetComponent<Rigidbody2D>().velocity = startingVelocity;
-
+        if (Input.GetButtonDown("Jump"))
+        {
+            GetComponent<Rigidbody2D>().velocity = startingVelocity;
+        }
     }
-    void GetOut()
-    {
+    void GetOut() {
         Debug.Log("You are out");
         lives = lives - 1;
+        livesValue.text = lives.ToString();
 
         transform.position = startingPosition;
         GetComponent<Rigidbody2D>().velocity = new Vector2();
@@ -37,8 +45,16 @@ public class Ball : MonoBehaviour {
         }
     }
 
-    void GameOver()
-    {
+    void GameOver() {
         gameover.SetActive(true);
+    }
+
+    public void YouBrokeABrick() {
+        points += 1;
+        pointsValue.text = points.ToString();
+        var bricksleft = FindObjectsOfType<Brick>().Length;
+        if(bricksleft == 0) {
+            youwin.SetActive(true);
+        }
     }
 }
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+    Rigidbody2D rigidBody;
+    public float speed = 5;
+
+	// Use this for initialization
+	void Start () {
+    rigidBody = GetComponent<Rigidbody2D>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var x = Input.GetAxisRaw("Horizontal");
+        x = x * speed;
+        rigidBody.velocity = new Vector2(x, 0);
+
+
+	}
+}
